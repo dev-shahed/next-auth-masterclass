0574aa674a6f6e840bd60cee47c677af54577cd9
dev-shahed <shahedthedev@gmail.com>
2025-01-16 17:52:03 +0600

    feat: user registrations

--- app/(auth)/register/action.ts
+++ app/(auth)/register/action.ts
@@ -1,9 +1,14 @@
 "use server";
 
-import { registerFormTypes, RegisterSchema } from "./../schema";
+import { db } from "@/db/drizzle";
 
-// here receive inputValue as data
+import { registerFormTypes, RegisterSchema } from "../schema";
+import { usersTable } from "@/db/schema";
+import { revalidatePath } from "next/cache";
+
+// This function registers the user
 export const registerUser = async (data: registerFormTypes) => {
+  // Validate the input data using the RegisterSchema
   const newUserValidation = RegisterSchema.safeParse({
     email: data.email,
     password: data.password,
@@ -13,15 +18,31 @@ export const registerUser = async (data: registerFormTypes) => {
   if (!newUserValidation.success) {
     return {
       error: true,
-      message:
-        newUserValidation.error.issues[0]?.message ?? "An Error occurred",
+      message: newUserValidation.error.issues[0]?.message ?? "An error occurred",
     };
   }
 
-  if (newUserValidation.success) {
+  // Insert the validated data into the database
+  try {
+    await db
+      .insert(usersTable)
+      .values({
+        email: data.email,
+        password: data.password,
+        createdAt: new Date(),
+        twoFactorSecret: "", 
+        twoFactorActivated: false,
+      });
+    // Trigger revalidation after the user is successfully registered
+    revalidatePath("/");
     return {
-        error: false,
-        message: "from submit"
-    }
+      error: false,
+      message: "Registered successfully",
+    };
+  } catch (error) {
+    return {
+      error: true,
+      message: error || "An error occurred during registration",
+    };
   }
 };
