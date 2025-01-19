4ee8fe611308cde7c4769fe1daac675f009e8905
dev-shahed <shahedthedev@gmail.com>
2025-01-19 22:22:54 +0600

    feat: resend setup to password reset has been integrated

--- app/(auth)/password-reset/action.ts
+++ app/(auth)/password-reset/action.ts
@@ -3,6 +3,7 @@ import { db } from "@/db/drizzle";
 
 import { passwordResetTokensTable, usersTable } from "@/db/schema";
 import { handleError } from "@/lib/handleError";
+import { mailer } from "@/lib/mail";
 import { isAuthenticated } from "@/lib/utils";
 import { randomBytes } from "crypto";
 import { eq } from "drizzle-orm";
@@ -38,4 +39,30 @@ export const ResetPassword = async (email: string) => {
         tokenExpiry,
       },
     });
+
+  const resetLink = `${process.env.SITE_BASE_URL}/password-update?token=${passwordResetToken}`;
+
+  await mailer.sendMail({
+    from: "test@resend.dev",
+    to: email, // Recipient's email address
+    subject: "Password Reset Request", // Subject line
+    text: `Dear User,
+  
+  You have requested to reset your password. Please click the link below to reset it:
+  
+   ${resetLink}
+  
+  If you did not make this request, you can safely ignore this email.
+  
+  Thank you,
+  The YourDomain Team
+  `,
+    html: `
+      <p>Dear User,</p>
+      <p>You have requested to reset your password. Please click the link below to reset it:</p>
+      <p><a href="${resetLink}" target="_blank">Reset Password</a></p>
+      <p>If you did not make this request, you can safely ignore this email.</p>
+      <p>Thank you,<br />The YourDomain Team</p>
+    `,
+  });
 };
